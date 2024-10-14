b3fa5e7f8b7372100786eb97800d2150c69fbbff
Pierre <pierre.zurbito@proton.me>
2024-10-14 19:36:42 +0800

    Added SQL query

--- Spark-Dataframe.py
+++ Spark-Dataframe.py
@@ -5,16 +5,38 @@ spark = SparkSession.builder \
     .getOrCreate()
 
 
-df_csv = spark.read.csv("data.csv", header=True, inferSchema=True)
-#Test printing table
-# df_id_course=df_csv.show(4)
-# print(df_id_course)
+df= spark.read.csv("data.csv", header=True, inferSchema=True)
 
-df_csv_by_type = df_csv.groupby('COURSE TYPE')
+#Grouping by Course type
+df_csv_by_type = df.groupby('COURSE TYPE')
 df_csv_by_type.count().show(4)
 
-df_csv_filtered_fees=df_csv.filter(df_csv["FEES"] > 5500000)
+#Filtering fees
+df_csv_filtered_fees=df.filter(df["FEES"] > 5500000)
 df_csv_filtered_fees.show(14)
 
-df_csv_renamed = df_csv.withColumnRenamed("FEES", "TUITION")
-df_csv_renamed.show(7)
+#Renaming 2 columns
+df_renamed= df.withColumnRenamed("FEES", "TUITION")
+df_renamed_2= df_renamed.withColumnRenamed("COURSE TYPE", "FIELD")
+df_renamed_2.show(7)
+
+#Importing for SQL query
+df_renamed_2.createOrReplaceTempView("PROGRAMS")
+
+#Question 1: How much is the most expensive course in each country?
+query_1 = "SELECT COUNTRY, max(TUITION) FROM PROGRAMS GROUP BY COUNTRY"
+test_df=spark.sql(query_1)
+test_df.show()
+
+#Question 2: What is the most expensive field?
+query_2 = "SELECT FIELD, max(TUITION) FROM PROGRAMS GROUP BY FIELD"
+test_df_2=spark.sql(query_2)
+test_df_2.show()
+
+#Question 3: What is the cheapest country to perform an MBA with?
+query_3 = "SELECT COUNTRY, min(TUITION) as Min_Tuition FROM PROGRAMS WHERE FIELD = 'MBA' GROUP BY COUNTRY ORDER BY Min_Tuition"
+test_df_3=spark.sql(query_3)
+test_df_3.show()
+
+#Stop the spark session
+spark.stop()
